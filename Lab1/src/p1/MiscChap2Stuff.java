ae95ffc000504b932e59e989dbc6dcff9bf1ac1b
Don Li <dli9431@gmail.com>
2014-11-27 14:42:59 -0800

    lab

---
+++ Lab1/src/p1/MiscChap2Stuff.java
@@ -0,0 +1,16 @@
+package p1;
+
+public class MiscChap2Stuff {
+    
+    public static void main(String[] args) {
+        double x = (double) (3/100); // sb 0.0
+        double y = (double) 3 / 100; // sb 0.03
+        double z = 3 / (double) 100; // sb 0.03
+        double w = 3 / 100; // sb 0.0
+        int i = (int) (3.0 / 100.0);
+        
+        final double avg = (double)(1 + 2 + 5 + 17)/4; // 6.0 or 6.25
+        System.out.println(avg);
+        
+    }
+}
